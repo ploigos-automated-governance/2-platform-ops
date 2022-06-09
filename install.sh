500e33b1690cbe043fb823cc5b8ae9121f71ca63
Dillon Winchell <dwinchel@redhat.com>
2022-06-09 17:22:26 -0400

    Automate sonarqube PV resize

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -ux -o pipefail
+set -eux -o pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
@@ -11,18 +11,22 @@ RETRY_WAIT=10
 while [ "${STATUS}" != 0 ] && [ $i -lt "${MAX_RETRIES}" ]; do
     ((i=i+1))
     echo $i
+    set +e
     oc apply -k ${SCRIPT_DIR}/components/
     STATUS=$?
+    set -e
     if [ "${STATUS}" != 0 ] && [ $i -lt "${MAX_RETRIES}" ]; then
         echo Not all resources were created. Waiting ${RETRY_WAIT} seconds for CRDs dependencies be created. ${i} retries left.
         sleep ${RETRY_WAIT}
     fi
 done
-
-if [ "${STATUS}" == 0 ]; then
-    echo "INSTALL SUCCESSFUL"
-else
-    echo "INSTALL FAILED! Check output for errors."
+if [ "${STATUS}" != 0 ]; then
+    echo "Resource creation failed! Check output for errors."
     exit 1
 fi
+
+oc patch pvc nexus-sonatype-nexus-data -p '{"spec":{"resources":{"requests":{"storage":"100Gi"}}}}' -n devsecops
+oc delete po -l app=sonatype-nexus -n devsecops
+
+echo "INSTALL SUCCESSFUL"
 
