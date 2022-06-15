bb9be6ab323e763dfe83baa9b63edad52c2e1645
Dillon Winchell <dwinchel@redhat.com>
2022-06-15 13:31:00 -0400

    Wait for sonarqube pvc to be created so resize will not fail

--- install-platform.sh
+++ install-platform.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
-set -eux -o pipefail
-
+set -eu -o pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# Script configuration
+VERBOSE=false # Set to "true" to have this script output debug information including the commands it is running.
 SONARQUBE_STORAGE_DESIRED=100Gi
-MAX_RETRIES=10
+MAX_RETRIES=30
 RETRY_WAIT=30
 
-# Create the k8s resources. Try until they are all created succesfully (CRDs may take time to exist, etc.)
+if [ "${VERBOSE}" ]; then set +x; fi
+
+# Create the k8s resources. Try until they are all created successfully (CRDs may take time to exist, etc.)
 STATUS=1
 i=0
 while [ "${STATUS}" != 0 ] && [ $i -lt "${MAX_RETRIES}" ]; do
     ((i=i+1))
-    echo $i
     set +e
     oc apply -k ${SCRIPT_DIR}/components/
     STATUS=$?
     set -e
     if [ "${STATUS}" != 0 ] && [ $i -lt "${MAX_RETRIES}" ]; then
-        echo Not all resources were created. Waiting ${RETRY_WAIT} seconds for CRDs dependencies be created. Retry #${i} of ${MAX_RETRIES}.
+        echo "Waiting for resource dependencies to be created. Attempting to create remaining resources in ${RETRY_WAIT} seconds. Retry ${i} of ${MAX_RETRIES}."
         sleep ${RETRY_WAIT}
     fi
 done
@@ -33,11 +33,31 @@ oc delete limitrange --all -n devsecops
 oc delete limitrange --all -n sigstore
 
 # Increase size of SonarQube PVC if needed. Requires restarting the sonarqube pod.
-SONARQUBE_STORAGE=$(oc get pvc nexus-sonatype-nexus-data -o yaml | yq .spec.resources.requests.storage)
+echo "Setting SonarQube PVC Size"
+echo "... Waiting for PVC to exist"
+STATUS=1
+i=0
+SONARQUBE_PVC_NAME=nexus-sonatype-nexus-data
+while [ "${STATUS}" != 0 ] && [ $i -lt "${MAX_RETRIES}" ]; do
+  set +e
+  oc get pvc "${SONARQUBE_PVC_NAME}" -o name
+  STATUS=$?
+  set -e
+  if [ "${STATUS}" != 0 ] && [ $i -lt "${MAX_RETRIES}" ]; then
+      echo "... Waiting for SonarQube PVC to be created. Checking again in ${RETRY_WAIT} seconds. Retry ${i} of ${MAX_RETRIES}."
+      sleep ${RETRY_WAIT}
+  fi
+done
+SONARQUBE_STORAGE=$(oc get pvc ${SONARQUBE_PVC_NAME} -o yaml | yq .spec.resources.requests.storage)
 if [ ${SONARQUBE_STORAGE} != ${SONARQUBE_STORAGE_DESIRED} ]; then
+    echo "... Updating pvc size to ${SONARQUBE_STORAGE_DESIRED}"
+    sleep 10 # Apparently this fails if you do it immediately after resource creation
     oc patch pvc nexus-sonatype-nexus-data -p '{"spec":{"resources":{"requests":{"storage":"100Gi"}}}}' -n devsecops
+    echo "... Restarting sonarqube Pod"
     oc delete po -l app=sonatype-nexus -n devsecops
+    echo "Sonarqube PVC Resized to ${SONARQUBE_STORAGE_DESIRED}"
+else
+  echo "SonarQube PVC is already sized correctly"
 fi
 
 echo "Installation Successful!"
-
